e3a98e3e326f4aa276761b9a037c0e71d4c34aca
Christian Bonsignore <christian.bonsignore@studenti.unicam.it>
2025-06-08 18:46:24 +0200

    Dataset downloaded

---
+++ download_dataset.py
@@ -0,0 +1,6 @@
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("ravirajsinh45/real-life-industrial-dataset-of-casting-product")
+
+print("Path to dataset files:", path)
